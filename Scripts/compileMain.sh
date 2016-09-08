433cbf2937cedab6f5e1075482dd175ccde9c4cd
TekuConcept <electro_madness@yahoo.com>
2016-09-08 18:43:51 +0000

    Updated Script Permissions

--- Scripts/compileMain.sh
+++ Scripts/compileMain.sh
@@ -3,11 +3,14 @@ scripts=$(pwd)
 cd ../Libraries/Drivers
 git submodule update --recursive
 
-cd ../BoneCentral/Peripherals
+cd ../../BoneCentral/Peripherals
 V8=$(node -e "var V8A=process.versions.v8.split('.');console.log('0x0'+V8A[0]+V8A[1]+V8A[2]);")
 swig -c++ -javascript -node -DV8_VERSION=$V8 swig_BoneCentral.i
 node-gyp configure
 node-gyp build
+
+#cd ../JavaScript
+#npm install httpdispatcher
 
 echo "- FINISHED -"
 cd $scripts
